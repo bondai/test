a45436bf25629ac7459e0a2fc31b6ab5f1aa9bbc
bondai <bondao@gmail.com>
2015-03-20 19:50:11 +0900

    Revert "動作テスト"
    This reverts commit b5353fdbbbf0fb51ad60361f54cebe04bba36612.

--- test.py
+++ test.py
@@ -6,31 +6,25 @@ while True:
     height = 0.0
     weight = 0.0
     bmi = 0.0
-    height = 170#raw_input("身長は？（cm）=")
+    height = raw_input("身長は？（cm）=")
     height = float(height)
     if height==0:
         print("END")
         break
     elif 100 <= height <=300:
         height /= 100
-    weight = 80#raw_input("体重は？（kg）=")
+    weight = raw_input("体重は？（kg）=")
     weight = float(weight)
     bmi = weight / (height * height)
     if bmi <= 18.5:
         print("痩せ形")
-        break
     elif bmi <= 25:
         print("普通体重")
-        break
     elif bmi <= 30:
         print("肥満（1度）")
-        break
     elif bmi <= 35:
         print("肥満（2度）")
-        break
     elif bmi <= 40:
         print("肥満（3度）")
-        break
     else:
         print("肥満（4度）")
-        break
